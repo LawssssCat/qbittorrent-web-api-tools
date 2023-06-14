fe0ec99909beb47f60e95782ba961031c6c2bba1
lawsssscat <lawsssscat@163.com>
2023-06-14 18:01:12 +0800

    refactor: test print and handle exception of fetching trackers

--- example/fetch_net_trackers.sh
+++ example/fetch_net_trackers.sh
@@ -10,12 +10,12 @@ source ../lib/qb.web-api.shlib
 echo "=========== urls ==========="
 : ${example__fetch_net_trackers__fetch_urls:="http://example_2.com http://example_3.com"}
 fetch_urls=(
+    $example__fetch_net_trackers__fetch_urls
     https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt # https://github.com/ngosang/trackerslist
     https://cf.trackerslist.com/best.txt # https://github.com/XIU2/TrackersListCollection
     https://newtrackon.com/api/stable # https://newtrackon.com/
     https://cdn.jsdelivr.net/gh/DeSireFire/animeTrackerList/AT_best.txt # https://github.com/DeSireFire/animeTrackerList
     http://example.com # test exception
-    $example__fetch_net_trackers__fetch_urls
 )
 echo "${fetch_urls[@]}" | tr " " "\n"
 
@@ -23,4 +23,11 @@ echo "=========== fetch ==========="
 fetch_net_trackers "${fetch_urls[@]}"
 
 echo "=========== result ==========="
-echo "$net_trackers" 
+echo "$qbt_net_trackers"
+
+echo "=========== exception ==========="
+exception_urls="$(echo "$qbt_net_exception_urls" | tr ' ' '\n' | grep -v -e 'example')"
+if [ -n "$exception_urls" ]; then
+    echo "$exception_urls"
+    exit 1
+fi
--- test/test-example.sh
+++ test/test-example.sh
@@ -5,6 +5,20 @@ COLOR_RED='\e[1;31m'
 COLOR_GREEN='\e[1;32m'
 COLOR_RESET='\e[0m'
 
+function print_example_title {
+  local text="$1"
+  local dots='...............................................................'
+  printf '%s %s  ' "${text}" "${dots:${#text}}"
+}
+
+function print_example_success {
+    echo -e "${COLOR_GREEN}SUCCESS${COLOR_RESET}"
+}
+
+function print_example_fail {
+    echo -e "${COLOR_RED}FAIL${COLOR_RESET}"
+}
+
 ############## context ############## 
 test_env_file="./set-env.sh" ; [ -f "$test_env_file" ] && source "$test_env_file"
 cd ../example
@@ -13,13 +27,14 @@ example_index=0
 example_exception=0
 example_list="$(ls -1 | grep -v "set-env.sh")"
 for example_name in $example_list; do
-    echo -n "EXAMPLE_${example_index}: $example_name ... "
-    bash ${example_name} 1>/dev/null
+    print_example_title " · EXAMPLE_${example_index}: $example_name"
+    example_result="$(bash $example_name 2>&1 1>/dev/null)"
     if [ $? -ne 0 ]; then
         ((example_exception++))
-        echo -e "${COLOR_RED}FAIL${COLOR_RESET}"
+        print_example_fail
+        [ -n "$example_result" ] && echo "$example_result"
     else
-        echo -e "${COLOR_GREEN}SUCCESS${COLOR_RESET}"
+        print_example_success
     fi
     ((example_index++))
 done
