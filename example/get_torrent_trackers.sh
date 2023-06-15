54ab6f3704bb6a147c3f751181f986a3e38be126
lawsssscat <lawsssscat@163.com>
2023-06-15 22:19:22 +0800

    enhance: add error message for getting torrent trackers.

--- example/get_torrent_trackers.sh
+++ example/get_torrent_trackers.sh
@@ -7,9 +7,23 @@ source ./set-env.sh
 source ../lib/qb.shlib
 source ../lib/qb.web-api.shlib
 
-echo "=========== raw ==========="
-: ${example__get_torrent_trackers__get_trackers:="1ff0a7472d8fc6c589a79ad2db6871cbfb0fab89"}
-get_torrent_trackers "$example__get_torrent_trackers__get_trackers" && echo "$qbt_torrent_trackers" 
+echo "=========== get torrents ==========="
+get_torrents && echo "$qbt_torrents" || exit 1
+
+echo "=========== get torrents hash ==========="
+qbt_torrent_hashs="$(echo "$qbt_torrents" | $jq_executable ".[].hash" -r)" || exit 1
+echo "$qbt_torrent_hashs"
+
+echo "=========== select a hash ==========="
+if [ "$(echo "$qbt_torrent_hashs" | wc -l)" -le 0 ]; then
+    echo "Please add a torrent first." >&2
+    exit 1
+fi
+qbt_torrent_hash="$(echo "$qbt_torrent_hashs" | head -n 1)"
+echo "$qbt_torrent_hash"
+
+echo "=========== get torrent trackers ==========="
+get_torrent_trackers "$qbt_torrent_hash" && echo "$qbt_torrent_trackers" || exit 1
 
 echo "=========== the list of url ==========="
 echo "$qbt_torrent_trackers" | $jq_executable ".[].url"
