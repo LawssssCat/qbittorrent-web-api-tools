8deb1cf73009fd1e08e5f65d247e2d045e60d977
lawsssscat <lawsssscat@163.com>
2023-06-20 23:04:36 +0800

    get_app_preferences set_app_preferences

--- example/ban_peers_clean.sh
+++ example/ban_peers_clean.sh
@@ -7,15 +7,34 @@ source ./set-env.sh
 source ../lib/qb.shlib
 source ../lib/qb.web-api.shlib
 
-echo "=========== set preference: add_trackers ==========="
-set_app_preferences '{"banned_IPs":""}' && echo "$qbt_webapi_response"
+echo "=========== set preference ==========="
+: ${example__ban_peers_clean__banned_IPs:-} # e.g. 0.0.0.11\n0.0.0.22
+echo "$example__ban_peers_clean__banned_IPs"
 
-echo "=========== get preference: add_trackers ==========="
-get_app_preferences && 
-qbt_qpp_banned_IPs="$(echo "$qbt_app_preferences" | $jq_executable ".banned_IPs" -r)" || exit 1
+echo "=========== set preference ==========="
+set_app_preferences '{"banned_IPs":"'$example__ban_peers_clean__banned_IPs'"}' && echo "Ok" || {
+    echo "response status:"
+    echo "$qbt_webapi_response_status"
+    echo "error message:"
+    echo "$qbt_webapi_response_error"
+    exit $EXIT_ERROR
+} >&2
+
+echo "=========== get preference ==========="
+get_app_preferences  || {
+    echo "response status:"
+    echo "$qbt_webapi_response_status"
+    echo "error message:"
+    echo "$qbt_webapi_response_error"
+    exit $EXIT_ERROR
+} >&2
+qbt_qpp_banned_IPs="$(echo "$qbt_webapi_response_body" | $jq_executable ".banned_IPs" -r)" || exit $EXIT_ERROR
 echo "\"$qbt_qpp_banned_IPs\""
 
-if [ "$(lines_number "$qbt_qpp_banned_IPs")" -ne "0" ]; then
-    echo "Fail to clean banned_IPS. remain=$(lines_number "$qbt_qpp_banned_IPs")"
-    exit 1
+echo "=========== check preference ==========="
+if [ "$qbt_qpp_banned_IPs" == "$(echo -e "$example__ban_peers_clean__banned_IPs")" ]; then
+    echo "Ok"
+else
+    echo "Fail" >&2
+    exit $EXIT_ERROR
 fi
